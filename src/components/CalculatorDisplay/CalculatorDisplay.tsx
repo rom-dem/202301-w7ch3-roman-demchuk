9620ee31c5ba58f995b9acc074bf0d9689b94a58
Róman Demchuk <roman.demchuk@gmail.com>
2023-02-22 23:27:10 +0100

    Add CalculatorDisplay component

--- src/components/CalculatorDisplay/CalculatorDisplay.tsx
+++ src/components/CalculatorDisplay/CalculatorDisplay.tsx
@@ -1,21 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+import { DisplayContainer } from "./CalculatorDisplayStyled";
 
 interface CalculatorDisplayProps {
   value: string;
 }
-
-const DisplayContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  width: 100%;
-  height: 100px;
-  font-size: 48px;
-  padding: 0 16px;
-  background-color: #fff;
-  color: #333;
-`;
 
 const CalculatorDisplay: React.FC<CalculatorDisplayProps> = ({ value }) => {
   return <DisplayContainer>{value}</DisplayContainer>;
